88676982ceb24fb3bcb874d69dd3712b247d8495
A.Cox <40004388+acox101010@users.noreply.github.com>
2019-03-19 15:22:28 -0400

    Add files via upload
    Updated the code for incorporation of a push button

--- HS_CS_Tracking.ino
+++ HS_CS_Tracking.ino
@@ -1,35 +1,33 @@
 //Include the Arduino Stepper Library
 #include <Stepper.h>
-#define delaytime 3
+
+#define delaytime 30
 #define pin1  8//these are the Arduino pins that we use to activate coils 1-4 of the stepper motor
 #define pin2  9
 #define pin3  10
 #define pin4  11
-// Define Constants
+
+// Define floats and integers
 const float STEPS_PER_REV = 32; // Number of steps per internal motor revolution 
 const float GEAR_RED = 64;//  Amount of Gear Reduction
 const float STEPS_PER_OUT_REV = STEPS_PER_REV * GEAR_RED;// Number of steps per geared output rotation
-int val; //Define optical switch_1 variable as integer
-int val_2; // Define optical switch_2 variable as integer
 const int inPin = 4; //Optical switch_1 to digitial pin 4 on arduino
 const int inPin2 = 5;//Optical switch_2 to digitial pin 5 on arduino
+int val; //Define optical switch_1 variable as integer
+int val_2; // Define optical switch_2 variable as integer
 int StepsRequired;
-// The pins used are 8,9,10,11 
-// Connected to ULN2003 Motor Driver In1, In2, In3, In4 
-// Pins entered in sequence 1-3-2-4 for proper step sequencing
-//int val_on = 0;
-//int val_off = 0;
-//const int iPin = 7; //digital input for turning on PB at pin 7
-//const int oPin = 5; //digital input for turning off PN at pin 5
+int pButton = 7; // declares arduino digital input 7 as a push button input
+int pbVal = 0; //variable for storing on button state
+
 
 Stepper steppermotor(STEPS_PER_REV, 8, 10, 9, 11);
  
 void setup(){
   pinMode(inPin, INPUT);
   pinMode(inPin2, INPUT);
+  pinMode(pButton, INPUT);
   Serial.begin(9600);
 }
-
 void Step_A(){
   digitalWrite(pin1, HIGH);//turn on coil 1 
   digitalWrite(pin2, LOW); 
@@ -60,40 +58,81 @@ void step_OFF(){
   digitalWrite(pin3, LOW); 
   digitalWrite(pin4, LOW); 
 }
+
+void pb_increment(){
+  pbVal = pbVal+1;
+}
+
 void loop(){
   val = digitalRead(inPin);
   val_2 = digitalRead(inPin2);
-  
-  if (val >= 1 && val_2 == 0){
-    while (val_2 == 0){
+  if(digitalRead(pButton) == HIGH){
+    delay(100);
+    if(digitalRead(pButton) == HIGH){
+          pb_increment();
+          Serial.println(pbVal);
+    }
+  }
+ //Homing Loop
+  if (pbVal == 2 && val != 1){
+    val = digitalRead(inPin);
+    while (val == 0){
       // Rotate CCW 
       val = digitalRead(inPin);
       val_2 = digitalRead(inPin2);
-      Step_A();
-      delay(delaytime);
-      Step_B();
-      delay(delaytime);
-      Step_C();
-      delay(delaytime);
       Step_D();
-      delay(delaytime);
-      Serial.println("In Counter CW loop");
+      delay(10);
+      Step_C();
+      delay(10);
+      Step_B();
+      delay(10);
+      Step_A();
+      delay(10);
+      Serial.println("Homing");
     }
+    pbVal = 0; //Resets pusbutton variable
   }
-  else if (val == 0 && val_2 >= 1){
-    while (val == 0){
-      val = digitalRead(inPin);
-      val_2 = digitalRead(inPin2);
-      Step_D();
-      delay(delaytime);
-      Step_C();
-      delay(delaytime);
-      Step_B();
-      delay(delaytime);
-      Step_A();
-      delay(delaytime);
-    }
-      // Rotate CW 1/3 turn 
-      Serial.println("In CW loop");
-       }
-    }
+  //Rotational Test Start
+  if (pbVal == 4 && val == 1){
+    if (val >= 1 && val_2 == 0){
+      while (val_2 == 0){
+        // Rotate CCW 
+        val = digitalRead(inPin);
+        val_2 = digitalRead(inPin2);
+        digitalRead(pbVal);
+        Step_D();
+        delay(delaytime);
+        Step_C();
+        delay(delaytime);
+        Step_B();
+        delay(delaytime);
+        Step_A();
+        delay(delaytime);
+        Serial.println("In Counter CW loop");
+       
+      if (val == 0 && val_2 >= 1){
+        while (val == 0){
+          val = digitalRead(inPin);
+          val_2 = digitalRead(inPin2);
+          digitalRead(pbVal);
+          Step_A();
+          delay(delaytime);
+          Step_B();
+          delay(delaytime);
+          Step_C();
+          delay(delaytime);
+          Step_D();
+          delay(delaytime);
+          // Rotate CW 1/3 turn 
+          Serial.println("In CW loop");
+          Serial.println(pbVal);
+      }
+     }
+Serial.println("Out of loop");
+   }
+   //   delay(1000);//1 second pause 
+  } 
+ }
+}
+
+ 
